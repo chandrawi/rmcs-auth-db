2fbd4d897384408a775b13182f0bf8ef6eec6ced
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-04-28 14:05:58 +0700

    Create Auth struct

---
+++ src/lib.rs
@@ -0,0 +1,73 @@
+use sqlx::Pool;
+use sqlx::mysql::{MySql, MySqlPoolOptions};
+use sqlx::types::chrono::{DateTime, Utc};
+
+pub struct Auth {
+    pub pool: Pool<MySql>,
+    options: AuthOptions
+}
+
+#[derive(Debug)]
+pub struct AuthOptions {
+    limit: u32,
+    with_description: bool,
+    order: Vec<OrderOption>
+}
+
+#[derive(Debug)]
+pub enum OrderOption {
+    IdAsc,
+    IdDesc,
+    NameAsc,
+    NameDesc
+}
+
+impl Default for AuthOptions {
+    fn default() -> Self {
+        AuthOptions { 
+            limit: 10000, 
+            with_description: false, 
+            order: vec![] 
+        }
+    }
+}
+
+impl Auth {
+
+    pub async fn new(host: &str, username: &str, password: &str, database: &str) -> Auth {
+        let url = format!("mysql://{}:{}@{}/{}", username, password, host, database);
+        Auth::new_with_url(&url).await
+    }
+
+    pub async fn new_with_url(url: &str) -> Auth {
+        let pool = MySqlPoolOptions::new()
+            .max_connections(100)
+            .connect(url)
+            .await
+            .expect(&format!("Error connecting to {}", url));
+        Auth {
+            pool,
+            options: AuthOptions::default()
+        }
+    }
+
+    pub fn new_with_pool(pool: Pool<MySql>) -> Auth {
+        Auth {
+            pool,
+            options: AuthOptions::default()
+        }
+    }
+
+    pub fn set_limit(mut self, limit: u32) {
+        self.options.limit = limit;
+    }
+
+    pub fn set_with_description(mut self, with_description: bool) {
+        self.options.with_description = with_description;
+    }
+
+    pub fn set_order(mut self, order: Vec<OrderOption>) {
+        self.options.order = order;
+    }
+
+}
