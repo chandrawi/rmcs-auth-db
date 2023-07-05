c7e84f46aaab860d7a2282cb6f9999f6ce3e3252
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-07-05 15:13:35 +0700

    Add auth_token in token migration

--- migrations/20230528094443_token.up.sql
+++ migrations/20230528094443_token.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `token` (
   `access_id` int(10) UNSIGNED NOT NULL,
-  `refresh_id` char(32) NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `refresh_token` char(32) NOT NULL,
+  `auth_token` char(32) NOT NULL,
   `expire` timestamp NULL DEFAULT current_timestamp(),
   `ip` varbinary(16) NOT NULL DEFAULT '\0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 ALTER TABLE `token`
-  ADD PRIMARY KEY (`access_id`),
-  ADD UNIQUE KEY `refresh_id` (`refresh_id`);
+  ADD PRIMARY KEY (`access_id`);
